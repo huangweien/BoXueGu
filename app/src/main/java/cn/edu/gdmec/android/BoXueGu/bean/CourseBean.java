cf3973284e260069cf2b5a960b3415c1eef3b8d1
huangweien <549966459@qq.com>
2017-12-27 18:58:30 +0800

    12.27 18.58

---
+++ app/src/main/java/cn/edu/gdmec/android/BoXueGu/bean/CourseBean.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.BoXueGu.bean;
+
+/**
+ * Created by ASUS on 2017/12/27.
+ */
+
+public class CourseBean {
+    public int id;
+    public String imgTitle;
+    public String title;
+    public String intro;
+    public String icon;
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/BoXueGu/bean/ExercisesBean.java
@@ -0,0 +1,20 @@
+package cn.edu.gdmec.android.BoXueGu.bean;
+
+/**
+ * Created by ASUS on 2017/12/27.
+ */
+
+public class ExercisesBean {
+    public int id;
+    public String title;
+    public String content;
+    public int background;
+    public int subjectId;
+    public String subject;
+    public String a;
+    public String b;
+    public String c;
+    public String d;
+    public int answer;
+    public int select;
+}
